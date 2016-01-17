99b56355abd45d3a639a9df02a44fbcaaec564a6
Chris <cs@weblogixx.de>
2016-01-17 22:59:52 +0100

    Added ability to parse package.json, added first implementation for reading license files

--- test/licensereader/indexTest.js
+++ test/licensereader/indexTest.js
@@ -1,26 +1,38 @@
 'use strict';
+const fs = require('fs');
 const expect = require('chai').expect;
+const sinon = require('sinon');
 const licenseReader = require('../../lib/licensereader');
 
 describe('when using the licensing functions', () => {
 
   describe('when using getLicenses to retrieve all available licenses', () => {
 
-    it('should get the license data from the csv file for the first time', (done) => {
-
-      licenseReader.getLicenses((err, result) => {
-
-        expect(err).to.be.null;
-        expect(result[0]).to.deep.equal({
-          name: '3dfx Glide License',
-          identifier: 'Glide',
-          shortIdentifier: 'Glide',
-          osiApproved: false,
-          regex: '/Glide|3dfx(.)Glide(.)License|Glide/i'
-        });
-        done();
-      });
+    let spy;
+    beforeEach(() => {
+      spy = sinon.spy(fs, 'readFileSync')
     });
 
+    afterEach(() => {
+      fs.readFileSync.restore();
+    });
+
+    const firstEntry = {
+      name: '3dfx Glide License',
+      identifier: 'Glide',
+      shortIdentifier: 'Glide',
+      osiApproved: false,
+      regex: '/Glide|3dfx(.)Glide(.)License|Glide/i'
+    };
+
+    it('should get the license data from the csv file for the first time', () => {
+      const result = licenseReader.getLicenses();
+      expect(result[0]).to.deep.equal(firstEntry);
+      expect(spy.callCount).to.equal(1);
+    });
+
+    it('should return the cached result when called a second time', () => {
+      expect(spy.callCount).to.equal(0);
+    });
   });
 });
--- test/licensescanner/indexTest.js
+++ test/licensescanner/indexTest.js
@@ -1,14 +1,53 @@
 'use strict';
 const expect = require('chai').expect;
+const path = require('path');
+const settings = require('../../lib/settings');
 const licenseScanner = require('../../lib/licensescanner');
+const mockPath = path.join(process.cwd(), 'test/mocks');
 
 describe('when using the license scanner to get the license list', () => {
 
   describe('when using getLicenses to get all available licenses in a project', () => {
 
-    it('should throw an error if the wanted directory cannot be found', () => {
+    it('should throw an error if the path is not given', () => {
       expect(licenseScanner.getLicenses).to.throw('no directory given');
     });
 
+    it('should throw an error if the wanted directory cannot be found', () => {
+      let fn = () => { licenseScanner.getLicenses('unavailable'); };
+      expect(fn).to.throw('directory not accessible');
+    });
+  });
+
+  describe('when using getLicenseByPackage to get the contents of package.json', () => {
+
+    it('should return the not found state if the file cannot be found', () => {
+      expect(licenseScanner.getLicenseByPackage('unavailable')).to.equal(settings.LICENSE_STATUS_NOT_FOUND);
+    });
+
+    it('should return the license found in package.json', () => {
+      const resultString = licenseScanner.getLicenseByPackage(`${mockPath}/package-json/license-string.json`);
+      expect(resultString).to.equal('MIT');
+
+      const resultObj = licenseScanner.getLicenseByPackage(`${mockPath}/package-json/license-obj.json`);
+      expect(resultObj).to.equal('MIT');
+    });
+
+    it('should return the license found in package.json', () => {
+      const result = licenseScanner.getLicenseByPackage(`${mockPath}/package-json/licenses.json`);
+      expect(result).to.deep.equal(['MIT', 'GPL']);
+    });
+  });
+
+  describe('when using getLicenseByFile to get the license from a list of files', () => {
+
+    it('should return the not found state if no file can be found', () => {
+      expect(licenseScanner.getLicenseByFile('unavailable')).to.equal(settings.LICENSE_STATUS_NOT_FOUND);
+    });
+
+    it('return the license found in the license in filesytem', () => {
+      const result = licenseScanner.getLicenseByFile(`${mockPath}/licenses/LICENCE`);
+      console.log(result);
+    });
   });
 });
